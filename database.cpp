8424fb66cee37aab9fe1a7756ec77fc2c3c051d9
e-stoddard <53491371+e-stoddard@users.noreply.github.com>
2022-11-27 21:22:35 -0600

    Add files via upload

---
+++ database.cpp
@@ -0,0 +1,80 @@
+/*
+ * Eric Stoddard
+ * Saraswi Vollala
+ * Dulguun Narmandakh
+ *
+ * CSCI 467 - PE1
+ *
+ */
+
+#include "database.h"
+
+// function definitions database class
+
+void database::new_stock(int amt)
+{
+	stock.push_back(amt);
+}
+
+void database::add_stock(int index, int amt)
+{
+	stock[index] += amt;
+}
+void database::remove_stock(int index, int amt)
+{
+	if (amt < get_stock(index))
+	{
+		stock[index] -= amt;
+	}
+	else 
+	{ 
+		cout << "Not enough in stock. Only " << stock[index] << " available" << endl; 
+	}
+}
+/*
+void database::new_order(string user_name, string prod_name, string quantity, string email, string addr)
+{
+
+
+	vector<string> temp;
+	temp.push_back(user_name);
+	temp.push_back(prod_name);
+	temp.push_back(quantity);
+
+	std::size_t pos1{};
+	std::stoi(prod_name, &pos1);
+	int prod_id = pos1;
+	string price = legacy_database::get_price(prod_id);
+	
+	std::size_t pos2{};
+	std::stoi(price, &pos2);
+        int price_calc = pos2;	
+	
+	std::size_t pos3{};
+	std::stoi(quantity, &pos3);
+	int amount = pos3;
+
+	int total_price = amount * price_calc;
+	string total = std::to_string(total_price);
+	temp.push_back(total);
+
+	temp.push_back(email);
+	temp.push_back(addr);
+	temp.push_back("Received");
+
+	orders.push_back(temp);
+}
+*/
+void database::display_all_orders()
+{
+	cout << "***All Orders***" << endl;
+	for (int i = 0; i < orders.size(); i++)
+	{
+		for (auto& elem : orders[i])
+		{
+			cout << elem << ", ";
+		}
+		cout << endl;
+	}
+}
+
---
+++ database.h
@@ -0,0 +1,52 @@
+/*
+ * Eric Stoddard
+ * Saraswi Vollala
+ * Dulguun Narmandakh
+ *
+ * CSCI 467 - PE1
+ *
+ */
+
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "legacy_database.h"
+
+using namespace std;
+
+class database : public legacy_database
+{
+	public:
+		database() {}	// constructor
+		void new_stock(int amt);	// for adding a new product
+		void add_stock(int index, int amt);
+		void remove_stock(int index, int amt);
+
+		int get_stock(int index) { return stock[index]; }
+		void show_stock(int index) { cout << stock[index]; }
+
+		//void new_order(string user_name, string prod_name, string quantity, string email, string addr);
+		
+		void add_order(vector<string> next) { orders.push_back(next); }
+
+		std::string get_order_id(int index) { return orders[index][0]; }	
+		std::string get_prod_name(int index) { return orders[index][1]; }	
+		std::string get_quantity(int index) { return orders[index][2]; }	
+		std::string get_total_price(int index) { return orders[index][3]; }	
+		std::string get_email(int index) { return orders[index][4]; }
+		std::string get_address(int index) { return orders[index][5]; }
+		std::string get_status(int index) { return orders[index][6]; }
+
+		void display_all_orders();
+
+		// other public functions
+	
+	private:
+		std::vector<std::vector<std::string>> orders;   // stores customer orders
+		std::vector<int> stock;		// index = part number, int = amount in stock	   
+};
+#endif
